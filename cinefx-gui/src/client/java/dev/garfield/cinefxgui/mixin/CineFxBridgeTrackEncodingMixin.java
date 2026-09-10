48c67dae1f28663d4e01ad9b52c2ec410c1aff95
GarfieldTV <123653414+GarfieldTVA@users.noreply.github.com>
2026-09-11 01:43:07 +0200

    Encode tracks through public CineFX keyframe accessors

---
+++ cinefx-gui/src/client/java/dev/garfield/cinefxgui/mixin/CineFxBridgeTrackEncodingMixin.java
@@ -0,0 +1,50 @@
+package dev.garfield.cinefxgui.mixin;
+
+import com.google.gson.JsonArray;
+import com.google.gson.JsonObject;
+import dev.garfield.cinefx.api.ColorTrack;
+import dev.garfield.cinefx.api.Keyframe;
+import dev.garfield.cinefx.api.ScalarTrack;
+import dev.garfield.cinefxgui.editor.CineFxBridge;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
+
+import java.util.Locale;
+
+/** Stops CineFX GUI from reflecting private track fields now that the public API exposes keyframes. */
+@Mixin(value = CineFxBridge.class, remap = false)
+public abstract class CineFxBridgeTrackEncodingMixin {
+    @Inject(method = "encodeScalarTrack", at = @At("HEAD"), cancellable = true)
+    private static void cinefxGui$encodeScalar(ScalarTrack track, double durationTicks, CallbackInfoReturnable<JsonObject> cir) {
+        JsonObject object = new JsonObject();
+        object.addProperty("$kind", "ScalarTrack");
+        JsonArray keys = new JsonArray();
+        for (Keyframe<Double> key : track.keyframes()) {
+            JsonObject item = new JsonObject();
+            item.addProperty("tick", key.tick());
+            item.addProperty("value", key.value());
+            item.addProperty("easing", key.easingToNext().name());
+            keys.add(item);
+        }
+        object.add("keys", keys);
+        cir.setReturnValue(object);
+    }
+
+    @Inject(method = "encodeColorTrack", at = @At("HEAD"), cancellable = true)
+    private static void cinefxGui$encodeColor(ColorTrack track, double durationTicks, CallbackInfoReturnable<JsonObject> cir) {
+        JsonObject object = new JsonObject();
+        object.addProperty("$kind", "ColorTrack");
+        JsonArray keys = new JsonArray();
+        for (Keyframe<Integer> key : track.keyframes()) {
+            JsonObject item = new JsonObject();
+            item.addProperty("tick", key.tick());
+            item.addProperty("value", String.format(Locale.ROOT, "#%08X", key.value()));
+            item.addProperty("easing", key.easingToNext().name());
+            keys.add(item);
+        }
+        object.add("keys", keys);
+        cir.setReturnValue(object);
+    }
+}
